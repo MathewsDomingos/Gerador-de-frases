ef01adbc25617da3b9bfa635accac75e3d9515c9
MathewsDomingos <67337603+MathewsDomingos@users.noreply.github.com>
2022-06-20 14:48:22 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -16,20 +16,12 @@ const respostas = [
 // clicar em fazer pergunta
 function gerarTexto() {
 
-  /*if(respostas == ""){
-    document.body.classList.remove('glassmorphism') 
-  }*/
-
   if(active == ""){
-    removerDisplay.classList.remove('glassmorphism');
+    removerDisplay.classList.remove('glassmorphism')
   }
 
-  const removerDisplay = document.getElementById('active');
-  removerDisplay.classList.add('glassmorphism');
-
-  const element = buttonPerguntar
-  
-  //"<div>" + buttonPerguntar + "</div>"
+  const removerDisplay = document.getElementById('active')
+  removerDisplay.classList.add('glassmorphism')
 
   // gerar numero aletorio
   const  totalRespostas = respostas.length
@@ -39,3 +31,47 @@ function gerarTexto() {
 
   elementoResposta.style.opacity = 1;
 }
+
+//Compartilhar no WhatsApp
+document.addEventListener("DOMContentLoaded", function() {
+  //conteúdo que será compartilhado: Título da página + URL
+  const conteudo = encodeURIComponent(document.title + " " + window.location.href);
+  //altera a URL do botão
+  document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
+}, false);
+
+//Compartilhar no Facebook
+document.addEventListener("DOMContentLoaded", function() {            
+  //altera a URL do botão
+  document.getElementById("facebook-share-btt").href = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(window.location.href);
+}, false);
+
+//Compartilhar no twitter     
+document.addEventListener("DOMContentLoaded", function() {
+  var url = encodeURIComponent(window.location.href);
+  var titulo = encodeURIComponent(document.title);
+  //var via = encodeURIComponent("usuario-twitter"); //nome de usuário do twitter do seu site
+  //altera a URL do botão
+  document.getElementById("twitter-share-btt").href = "https://twitter.com/intent/tweet?url="+url+"&text="+titulo;
+}, false);
+
+//Compartilhar no Linkedin
+document.addEventListener("DOMContentLoaded", function() {
+  var url = encodeURIComponent(window.location.href); //url
+  var titulo = encodeURIComponent(document.title); //título
+  var linkedinLink = "https://www.linkedin.com/shareArticle?mini=true&url="+url+"&title="+titulo;
+  
+  //tenta obter o conteúdo da meta tag description
+  var summary = document.querySelector("meta[name='description']");
+  summary = (!!summary)? summary.getAttribute("content") : null;
+  
+  //se a meta tag description estiver ausente...
+  if(!summary){
+      //...tenta obter o conteúdo da meta tag og:description
+      summary = document.querySelector("meta[property='og:description']");
+      summary = (!!summary)? summary.getAttribute("content") : null;
+  }
+  //altera o link do botão
+  linkedinLink = (!!summary)? linkedinLink + "&summary=" + encodeURIComponent(summary) : linkedinLink;
+  document.getElementById("linkedin-share-btt").href = linkedinLink;
+}, false);
